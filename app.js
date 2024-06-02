17759e2f6812e26166e3fe07e18d76c068f0f9f1
sharma-dikshant <dikshantsharma2005@gmail.com>
2024-06-02 23:16:27 +0530

    completed the search feature and basic styling

---
+++ app.js
@@ -0,0 +1,38 @@
+const form = document.querySelector("#search form");
+const showContainer = document.querySelector(".show-container");
+form.addEventListener("submit", async (evt) => {
+  evt.preventDefault();
+
+  const images = showContainer.getElementsByTagName("img");
+  if (images) {
+    if (images.length > 0) {
+      while (images.length > 0) {
+        images[0].parentNode.removeChild(images[0]);
+      }
+    }
+  }
+
+  const submitbtn = this.document.querySelector(
+    `.search button[value="submit"]`
+  );
+  const formdata = new FormData(form, submitbtn);
+  const query = formdata.get("query");
+  try {
+    const response = await axios.get(
+      `https://api.tvmaze.com/search/shows?q=${query}`
+    );
+    console.log(response.data);
+    console.log();
+    showImages(response.data);
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+function showImages(data) {
+  for (const iterator of data) {
+    const imageEle = this.document.createElement("img");
+    imageEle.src = iterator.show.image.medium;
+    showContainer.append(imageEle);
+  }
+}
